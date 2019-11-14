e92cf98279b1e5eeda1d2396a1b55b0761299476
abhijgk <52520741+abhijgk@users.noreply.github.com>
2019-11-14 15:00:57 +0530

    Update flyodalgo.py

--- flyodalgo.py
+++ flyodalgo.py
@@ -30,4 +30,25 @@ for i in range(0,n+1):
 print("matrix after applying flyod's algorithm is:")
 flyodalg(arr,4)
 
+arr1=[[0   ,9999,1   ,1   ,8   ],
+     [6   ,0   ,3   ,2   ,9999],
+     [9999,9999,0   ,4   ,9   ],
+     [7   ,9999,2   ,0   ,3   ],
+     [3   ,8   ,9999,9999,0   ],
+    ]
+print()
+print("given matrix is:")
+for i in range(0,n+1):
+        for j in range(0,n+1):
+            print(" ",arr1[i][j],end="")
+        print("")
+print("matrix after applying flyod's algorithm is:")
+flyodalg(arr1,4)
+
+
+
+
+
+
+
 
